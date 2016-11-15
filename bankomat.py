76d0c08cbbbd2440e1b1b9ad9a07f7e0dc13c710
pekacc <mi.petkac@gmail.com>
2016-11-15 22:18:27 +0100

    Added functions, code is given away from main loop
    Added few features (cash on account,cash can't be lower than 0)

--- bankomat.py
+++ bankomat.py
@@ -5,40 +5,59 @@ class Banka:
         ucet.pokusy = pokusy
 ucty = list()
 ucty.append(Banka(1234,0,3))
+
+def pristup(i):
+    while ucty[i].pokusy > 0 and ucty[i].pokusy < 4:
+        vstup = int(input("Zadajte heslo: "))
+        if int(vstup) == ucty[i].heslo:
+            print("Pristup povoleny!")
+            ucty[i].pokusy = 4;
+        else:
+            ucty[i].pokusy -= 1;
+            print("Nespravne heslo!")
+            print("Pocet zostavajucich pokusov: " + str(ucty[i].pokusy))
+    if ucty[i].pokusy == 0:
+        print("Zablokovany ucet!")
+        return 0
+    else:
+        ucty[i].pokusy = 3
+        return 1
+
+def vUcte():
+    i = int(input("Zadajte cislo uctu: "))
+    akcia = pristup(i) 
+    if akcia == 0: return
+    while akcia > 0:
+        akcia = int(input("1.Vlozit peniaze\n2.Vybrat peniaze\n3.Pozriet zostatok\n0.Ukoncit\n"))
+        if akcia == 0: return
+        if akcia == 1:
+            suma = int(input("Zadajte sumu: "))
+            ucty[i].suma += suma
+            print("Zostatok na ucte: " + str(ucty[i].suma) + "€\n")
+        elif akcia == 2:
+            suma = int(input("Zadajte sumu: "))
+            if ucty[i].suma < suma:
+                input("Nedostatocny kapital, operacia prerusena!\n")
+            else:
+                ucty[i].suma -= suma
+                print("Zostatok na ucte: " + str(ucty[i].suma) + "€\n")
+        elif akcia == 3:
+            print("Zostatok na ucte: " + str(ucty[i].suma) + "€\n")
+            input()
+
+def vytvor():
+    print("Vase cislo: " + str(len(ucty)))
+    heslo = int(input("Zadajte Vas pin kod: "))
+    ucty.append(Banka(heslo,0,3))
+    print("Ucet uspesne vytvoreny!")
+
 while(1):
     print("Vitajte v Banke")
     akcia = int(input("1. Vstupit do uctu \n2. Vytvorit ucet\n0. ukoncit\n"))
     if akcia == 1:
-        i = int(input("Zadajte cislo uctu: "))
-        while ucty[i].pokusy > 0 and ucty[i].pokusy < 4:
-            vstup = int(input("Zadajte heslo: "))
-            if int(vstup) == ucty[i].heslo:
-                print("Pristup povoleny!")
-                ucty[i].pokusy = 4;
-            else:
-                ucty[i].pokusy -= 1;
-                print("Nespravne heslo!")
-                print("Pocet zostavajucich pokusov: " + str(ucty[i].pokusy))
-        if ucty[i].pokusy == 0:
-            print("Zablokovany ucet!")
-        else:
-            if akcia == 0: break
-            ucty[i].pokusy = 3
-            while akcia > 0:
-                print("Zostatok na ucte: " + str(ucty[i].suma) + "€\n")
-                akcia = int(input("1.Vlozit peniaze\n2.Vybrat peniaze\n0.exit\n"))
-                if akcia == 0: break
-                suma = int(input("Zadajte sumu: "))
-                if akcia == 1:
-                    ucty[i].suma += suma
-                elif akcia == 2:
-                    ucty[i].suma -= suma
+        vUcte()
     elif akcia == 2:
-        print("Vase cislo: " + str(len(ucty)))
-        heslo = int(input("Zadajte Vas pin kod: "))
-        ucty.append(Banka(heslo,0,3))
-        print("Ucet uspesne vytvoreny!")
+        vytvor()
     elif akcia == 0:
         break
 print("Dovidenia!")
-        
